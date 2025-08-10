5e3c3a1939d1faaa923ba309ec3d60c91f6b0d3a
chagasma <chagasma.dev@gmail.com>
2025-08-10 13:55:37 -0300

    :hammer: feat: add simple client files

---
+++ src/agents/client/prompts.py
@@ -0,0 +1,6 @@
+from textwrap import dedent
+
+CLIENT_PROMPT = dedent(f'''
+Você é um cliente da Auria AI que está recebendo um atendimento.
+Informe as informações necessárias corretamente ao atendente.
+''')
