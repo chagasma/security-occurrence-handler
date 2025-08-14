67ea8cbddfaa52a4d9230a6a3f3c440c64ebba9b
chagasma <chagasma.dev@gmail.com>
2025-08-14 15:02:02 -0300

    :hammer: feat: improve code modularity

--- src/agents/client/states.py
+++ src/agents/client/states.py
@@ -1,12 +1,5 @@
-from typing import Annotated
-
-from langchain_core.messages import AnyMessage
-from langgraph.graph import add_messages
-from pydantic import BaseModel
+from src.agents.core.states import MessagesState
 
 
-class ClientState(BaseModel):
-    messages: Annotated[list[AnyMessage], add_messages]
-
-    def __getitem__(self, item):
-        return getattr(self, item, None)
+class ClientState(MessagesState):
+    pass
